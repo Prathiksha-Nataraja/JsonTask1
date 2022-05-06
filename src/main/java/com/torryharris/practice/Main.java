2223e269f4cf93a22c26cfa2d6e3bdea16117cc5
Prathiksha Nataraja <prathiksha_nataraja@thbs.india.com>
2022-05-06 14:33:20 +0530

    iteration

--- src/main/java/com/torryharris/practice/Main.java
+++ src/main/java/com/torryharris/practice/Main.java
@@ -1,5 +1,6 @@
 package com.torryharris.practice;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 import java.util.LinkedHashMap;
@@ -8,15 +9,24 @@ import java.util.Map;
 public class Main {
 
 
-  public static void json(){
-    JsonObject js =new JsonObject();
-    js.put("fullname","Kathleen Brown");
-    Map m1 = new LinkedHashMap(2);
-    m1.put("street","23505 Stewart Track");
-    m1.put("city","Mandiana");
-    js.put("address",m1);
-    System.out.println(js.encodePrettily());
-  }
+  public static void json() {
+    JsonObject js = new JsonObject();
+      js.put("fullname", "Kathleen Brown");
+      Map m1 = new LinkedHashMap(2);
+      m1.put("street", "23505 Stewart Track");
+      m1.put("city", "Mandiana");
+      js.put("address", m1);
+    Map m2 = new LinkedHashMap(3);
+    m2.put("PAN", "AMJXH1456H");
+    m2.put("Adhar", "8889 8856 3338 2234");
+    m2.put("employee id", "[\"123\",\"234\",\"567\"]");
+
+    js.put("ids", m2);
+    for(int i=0;i<js.size() ;i++) {
+      System.out.println(js.encodePrettily());
+    }
+    }
+
   public static void main(String[] args){
     Main main = new Main();
     json();
